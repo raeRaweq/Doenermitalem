fb7013f21769e29f86a9bf6f4a4854fe799ba268
raeRaweq <66205353+raeRaweq@users.noreply.github.com>
2020-06-04 22:15:35 +0200

    upload 1

---
+++ index.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+const PREFIX  = '#';
+
+var version = '1.0'
+
+bot.on('ready', () =>{
+    console.log('ein mal Döner mit allem bitte!')
+})
+
+bot.on('message', message=>{
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'youtube':
+            message.reply('Raweq auf YouTube : https://www.youtube.com/channel/UC0AmoR_u5UVHaJF_AMccZHg');
+            break;
+        case 'bewerben':
+            message.reply('Hier kannst du dich bewerben: https://forms.gle/5JEdANX2RHVXHQDc7');
+            break;
+        case 'info':
+            if(args[1] === 'version'){
+                message.channel.send('Version ' + version + ' by Raweq');
+            }else{
+                message.reply('ungültiger Befehl')
+            }
+            break;
+        case 'clear':
+            if(!args[1]) return message.reply ('Ups! Es ist ein Fehler unterlaufen!(#clear [nummer]')
+            message.channel.bulkDelete(args[1]);
+            break;
+        
+
+    }
+})
+
+bot.login(process.env.token);
